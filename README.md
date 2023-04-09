6c0402ad99b63fd57911eb9c8b1b1294950ca4a8
Edson Martins <aekmartins@gmail.com>
2023-04-08 23:43:30 -0300

    change example

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cloud-functions
+
+How to run:
+
+```
+gcloud functions deploy go-http-function \                                        
+--gen2 \
+--runtime=go120 \
+--region=us-central1 \
+--source=. \
+--entry-point=HelloGet \
+--trigger-http \
+--allow-unauthenticated
+```
--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,17 @@
 package helloworld
 
 import (
-	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 func init() {
-	functions.HTTP("HelloHTTP", HelloHTTP)
+	functions.HTTP("HelloGet", helloGet)
 }
 
-// HelloHTTP is an HTTP Cloud Function with a request parameter.
-func HelloHTTP(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World!")
-		return
-	}
-	if d.Name == "" {
-		fmt.Fprint(w, "Hello, World!")
-		return
-	}
-	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+// helloGet is an HTTP Cloud Function.
+func helloGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
 }
